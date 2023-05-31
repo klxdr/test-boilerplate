b41bb96ad0a18dbbda08b4e0b5a1d210f8cb2934
AlexKotov <kotovaleksandr@gmail.com>
2023-05-31 09:22:12 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import List from './components/List.vue';
+</script>
+
+<template>
+  <main>
+    <router-view />
+  </main>
+  <list></list>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+    export interface contact {
+        firstName: string,
+        lastName: string,
+        age: number
+    }
+
+    const contacts: contact[] = [
+        {
+            firstName: "Ivan",
+            lastName: "Ivanov",
+            age: 23
+        },
+        {
+            firstName: "Petr",
+            lastName: "Petrov",
+            age: 25
+        },
+        {
+            firstName: "Sidor",
+            lastName: "Sidorov",
+            age: 26
+        },
+    ];
+
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="contact in contacts">
+        {{ contact.firstName }} {{ contact.lastName }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
